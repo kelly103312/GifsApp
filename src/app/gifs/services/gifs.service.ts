fb5684ac85fd0147e1493c8600302cbae37963fe
kquintana <kquintana@actualicese.com>
2022-04-25 16:13:10 -0500

    Implementación con efectos de imagen y demás, version 1

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Gif, SearchGif } from '../interfaces/gifs.interface';
 
@@ -6,9 +6,9 @@ import { Gif, SearchGif } from '../interfaces/gifs.interface';
   providedIn: 'root'
 })
 export class GifsService {
-  //vYg5CY6t2dEqrw3d7B0bOeVyIYv07qPn
   private _historial: string[] =[]; 
-  
+  private api_key : string = 'LgFbmmSzob9GkgHvMC6nGTcxeEOKPAFl'
+  private service_url : string = 'https://api.giphy.com/v1/gifs'
   public resultados: Gif[] = [];
   
   get historial() : string[]{
@@ -31,23 +31,20 @@ export class GifsService {
         //almacena en el storage de js el historial de busquedas, para ello se esa json => toma un objeto y lo convierte en string
         localStorage.setItem('historial', JSON.stringify(this._historial));
       }
+      const params = new HttpParams()
+         .set('api_key' ,  this.api_key)
+         .set('limit' , '10')
+         .set('q' , query);
+
+
       //modulo de angular que retorna observabols permite además agregar funcionalidades entre otras
       //La interface se obtiene del resultado de la api en la pagina https://app.quicktype.io/
-      this.http.get<SearchGif>(`https://api.giphy.com/v1/gifs/search?api_key=LgFbmmSzob9GkgHvMC6nGTcxeEOKPAFl&q=${query}&limit=10`)
+      this.http.get<SearchGif>(`${this.service_url}/search?`, {params})
           //Se ejecuta cuando se obtiene una respuesta y se almacena en resp
           .subscribe( (resp) =>{
-            console.log(resp.data);
             this.resultados=resp.data;
             localStorage.setItem('resultados', JSON.stringify( this.resultados));
           });
   }
 
-  
-
-  //fetch('https://api.giphy.com/v1/gifs/search?api_key=LgFbmmSzob9GkgHvMC6nGTcxeEOKPAFl ball.
-  //Then( resp => {
-  //  resp.json().then(data =>{
-  //    console.log(data);
-  //  })})
-  
 }
